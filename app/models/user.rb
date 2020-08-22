1833ab24a0554062a2edd316053de798ba5176cf
ginga <poorfornelius@gmail.com>
2020-08-22 13:04:13 +0000

    seventh commit

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+   validates :name, presence: true, length: { in: 1..15 }
+   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+   validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }
+   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+   validates :password, length: { in: 8..32 }, format: { with: VALID_PASSWORD_REGEX }
+   
+   has_secure_password
 end
+
