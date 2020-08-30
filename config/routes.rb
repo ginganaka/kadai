65976601bd71974809e1bcf904f0fc7498fbebee
ginga <poorfornelius@gmail.com>
2020-08-30 17:06:16 +0000

    eighth commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :users
-
-  get 'pages/index'
+  get 'sessions/new'
 
   root 'pages#index'
   get 'pages/help'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users
+
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 end
